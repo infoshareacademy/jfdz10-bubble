046c931acba98a28c923ad7eacf9c451a9ee2a06
crchs <kolenycz@gmail.com>
2018-11-27 17:55:10 +0100

    full responsitivity, added header, photo function

---
+++ js/team.js
@@ -0,0 +1,31 @@
+function team1Hover(element) {
+    element.setAttribute('src', '/images/borys2.png');
+  }
+  
+  function team1Unhover(element) {
+    element.setAttribute('src', '/images/borys1.png');
+  }
+
+  function team2Hover(element) {
+    element.setAttribute('src', '/images/witek2.png');
+  }
+  
+  function team2Unhover(element) {
+    element.setAttribute('src', '/images/witek1.png');
+  }
+
+  function team3Hover(element) {
+    element.setAttribute('src', '/images/mikolaj2.png');
+  }
+  
+  function team3Unhover(element) {
+    element.setAttribute('src', '/images/mikolaj1.png');
+  }
+
+  function team4Hover(element) {
+    element.setAttribute('src', '/images/kasia2.png');
+  }
+  
+  function team4Unhover(element) {
+    element.setAttribute('src', '/images/kasia1.png');
+  }
