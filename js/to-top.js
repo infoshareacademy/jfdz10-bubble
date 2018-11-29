0b6e0e5c23607b124642ff0b101ce2c751d3c952
crchs <kolenycz@gmail.com>
2018-11-29 20:06:43 +0100

    aded smooth scrolling to the page

--- js/to-top.js
+++ js/to-top.js
@@ -5,20 +5,15 @@ $("#to-top").hide();
 
 // fade in #back-top
 $(function () {
-    $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-    $('#to-top').fadeIn();
-    } else {
-    $('#to-top').fadeOut();
-    }
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            $('#to-top').fadeIn();
+        }   
+        else {
+            $('#to-top').fadeOut();
+        }
     });
 
-    // scroll body to 0px on click
-    $('#to-top__link').click(function () {
-    $('body,html').animate({
-    scrollTop: 0}, 800);
-    return false;
-    });
     });
 
 });
