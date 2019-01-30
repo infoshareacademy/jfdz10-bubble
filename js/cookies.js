66590e971f97932a611c9a0f3b44e1a7ab0cc34f
WitoG <wit1godlewski@gmail.com>
2019-01-30 22:25:29 +0100

    Cookies done

--- js/cookies.js
+++ js/cookies.js
@@ -3,12 +3,25 @@ var container = document.querySelector('#cookies');
 
 function setCookie() {
     var now = new Date();
-    var expirationDateInMS = 30*24*60*60*1000 
+    var expirationDateInMS = 30*24*60*60*1000; 
     now.setTime(now.getTime() + expirationDateInMS);
-    var expires = "expires="+ now.toUTCString();
-    document.cookie = "expiration=" + expires;
+    var expires = 'expires=' + now.toUTCString();
+    document.cookie = 'expiration=' + expires;
 
     container.style.display = 'none';
   }
 
   closeBtn.addEventListener('click', setCookie);
+
+  
+  
+  function cookieExists() {
+    return document.cookie.includes('expiration');
+  }
+
+  if (!cookieExists()) {
+    container.style.display = 'block';
+    
+ }
+
+ 
