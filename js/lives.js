7acddc4d6ed59a2e7d42abb2b217afa9e4631e94
WitoG <wit1godlewski@gmail.com>
2019-02-04 22:50:40 +0100

    -

---
+++ js/lives.js
@@ -0,0 +1,9 @@
+var lives=3;
+function checkLives() {
+    if (checkIfGoalScored()){
+        console.log('woow');
+        //wyczysc wszystkie inne funkcje (zakoncz gre)
+        //wyswietl formularz do wpisania inicjalow 
+        // odpal checkIfScoreBetter(inicjaly z powyzej, punkty)
+    }
+}
