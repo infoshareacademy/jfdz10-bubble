6995e068378b53b383321f159eaae230012335f4
mik5owski <piatkowskimikolaj@gmail.com>
2019-02-09 14:58:42 +0100

    bad branch

--- js/instructions.js
+++ js/instructions.js
@@ -19,3 +19,24 @@ function showInstructions() {
 };
 
 instructionsBtn.addEventListener('click', showInstructions);
+
+
+backgroundMusic = new Audio('always.mp3');
+backgroundMusic.play();
+
+let musicBtn = document.querySelector('.music');
+let musicOn = true;
+
+function playPause() {
+    if (musicOn) {
+        backgroundMusic.pause();
+        musicOn = false;
+        musicBtn.style.backgroundImage = 'url(/images/musicOff.png)';
+    } else {
+        backgroundMusic.play();
+        musicOn = true;
+        musicBtn.style.backgroundImage = 'url(/images/musicOn.png)';
+    }
+}
+
+musicBtn.addEventListener('click', playPause);
